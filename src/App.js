c42b2d12ab236075af117c428f0b41c0d6c402b4
Akhil <akhilvij@gmail.com>
2019-11-24 12:42:10 +0530

    Adding onChange for Manual Analysis

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { TweetTable } from "./components/TweetTable.js";
+import TextField from "@material-ui/core/TextField";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = [
+      {
+        text:
+          "Some random text example goes here. Some random text example goes here.Some random text example goes here.Some random text example goes here.Some random text example goes here."
+      }
+    ];
+    this.handleEnter = this.handleEnter.bind(this);
+  }
+  handleEnter(event) {
+    if (event.keyCode === 13) {
+      let textValue = event.target.value;
+      // Run the model on the textValue
+      this.setState([
+        {
+          text: textValue
+        }
+      ]);
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <TweetTable tweets={this.state} />
+        <div className="manual-analysis-cont">
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Enter any text and press Enter"
+            variant="outlined"
+            onChange={this.handleChange}
+            onKeyDown={this.handleEnter}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
