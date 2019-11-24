a5f5d97aaf07fc42a18ee563e2bc9d9424c28b96
Akhil <akhilvij@gmail.com>
2019-11-25 00:30:30 +0530

    Updates

---
+++ precache-manifest.5aadc9b439031860167259ec077527a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addfe838dc7fe0719aa54e573c44678a",
+    "url": "/tweet-toxicity/index.html"
+  },
+  {
+    "revision": "57845ff327ff09ee8ddc",
+    "url": "/tweet-toxicity/static/css/main.ddf6aea9.chunk.css"
+  },
+  {
+    "revision": "c2dc2c9062664676c1c6",
+    "url": "/tweet-toxicity/static/js/2.8525a2b7.chunk.js"
+  },
+  {
+    "revision": "57845ff327ff09ee8ddc",
+    "url": "/tweet-toxicity/static/js/main.7adb9915.chunk.js"
+  },
+  {
+    "revision": "040ff58ff322ee072da5",
+    "url": "/tweet-toxicity/static/js/runtime-main.80e93534.js"
+  }
+]);
